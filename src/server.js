3ef87a67c54243c26cb665a283f675ca83c67bce
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2024-09-20 02:40:51 +0100

    rota para obter as imagens

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require("express-async-errors")
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
 const express = require("express")
-
 const routes = require("./routes")
 
 migrationsRun()
@@ -11,8 +11,9 @@ migrationsRun()
 const app = express()
 app.use(express.json())
 
-app.use(routes)
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
+app.use(routes)
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
